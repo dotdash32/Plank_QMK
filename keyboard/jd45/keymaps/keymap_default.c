2a38b496d478600765f4186584af71a839219c79
jur0kuban <jared.a.jennings@gmail.com>
2015-11-11 22:48:46 -0600

    Update keymap_default.c

--- keyboard/jd45/keymaps/keymap_default.c
+++ keyboard/jd45/keymaps/keymap_default.c
@@ -1,5 +1,5 @@
 #include "jd45.h"
-#include "backlight.h"
+//#include "backlight.h"
 
 /* this keymap is to provide a basic keyboard layout for testing the matrix
  * for more practical and complicated keymap refer to other keymaps in the same folder
@@ -7,25 +7,37 @@
 /* JD45 keymap definition macro
  */
 #define KEYMAP( \
-    K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, K13, \
-	K14, K15, K16, K17, K18, K19, K20, K21, K22, K23, K24, K25, \
-	K26, K27, K28, K29, K30, K31, K32, K33, K34, K35, K36, K37, \
-	K38, K39, K40, K41, K42, K43, K44, K45, K46, K47 \
+    K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, \
+	K13, K14, K15, K16, K17, K18, K19, K20, K21, K22,      K23, \
+	K24, K25, K26, K27, K28, K29, K30, K31, K32, K33, K34, K35, \
+	K36, K37, K38, K39,      K40,           K41, K42, K43, K44, \
 ) { \
-    { KC_##K01, KC_##K02, KC_##K03, KC_##K04, KC_##K05, KC_##K06, KC_##K07, KC_##K08, KC_##K09, KC_##K10, KC_##K11, KC_##K12, KC_##K13 }, \
-    { KC_##K14, KC_##K15, KC_##K16, KC_##K17, KC_##K18, KC_##K19, KC_##K20, KC_##K21, KC_##K22, KC_##K23, KC_##K24, KC_##K25, KC_NO    }, \
-    { KC_##K26, KC_##K27, KC_##K28, KC_##K29, KC_##K30, KC_##K31, KC_##K32, KC_##K33, KC_##K34, KC_##K35, KC_##K36, KC_##K37, KC_NO    }, \
-    { KC_##K38, KC_##K39, KC_##K40, KC_##K41, KC_##K42, KC_NO,    KC_##K43, KC_NO,    KC_##K44, KC_##K45, KC_##K46, KC_##K47, KC_NO    }  \
+    { KC_##K01, KC_##K02, KC_##K03, KC_##K04, KC_##K05, KC_##K06, KC_##K07, KC_##K08, KC_##K09, KC_##K10, KC_##K11, KC_##K12 }, \
+    { KC_##K13, KC_##K14, KC_##K15, KC_##K16, KC_##K17, KC_##K18, KC_##K19, KC_##K20, KC_##K21, KC_##K22, KC_NO,    KC_##K23 }, \
+    { KC_##K24, KC_##K25, KC_##K26, KC_##K27, KC_##K28, KC_##K29, KC_##K30, KC_##K31, KC_##K32, KC_##K33, KC_##K34, KC_##K35 }, \
+    { KC_##K36, KC_##K37, KC_##K38, KC_##K29, KC_NO,    KC_##K40, KC_NO,    KC_NO,    KC_##K41, KC_##K42, KC_##K43, KC_##K44 }  \
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = KEYMAP(
-  ESC,   Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,     QUOT, BSPC,
-  TAB,   A,    S,    D,    F,    G,    H,    J,    K,    L,    SCLN,  ENT,
-  LSFT,  Z,    X,    C,    V,    B,    N,    M,    COMM, DOT,  SLSH,  RSFT,
-  PAUSE, LCTL, LALT, DEL,  SPC,  DEL,  LEFT, UP,   DOWN, RIGHT ),
+  KC_ESC,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
+  KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,       KC_ENT,
+  KC_CAPS, KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_RSFT,
+  KC_LCTL, KC_LALT, KC_LGUI,  KC_FN1, KC_SPC,  KC_FN2,  KC_SCOLON, KC_QUOTE,   KC_SLSH ),
+[1] = KEYMAP(
+  KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL,
+  KC_TRNS, KC_HOME, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_EQL, KC_MINS, KC_LBRACKET, KC_RBRACKET, KC_BSLS
+  KC_TRNS, KC_TRNS, KC_END, KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ),
+[2] = KEYMAP( 
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT ),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-
+	[0] = ACTION_LAYER_MOMENTARY(1),
+	[1] = ACTION_LAYER_MOMENTARY(2),
 };
+
